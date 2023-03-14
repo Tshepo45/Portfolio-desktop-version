80e5649edf873bcc13a7910f5c47a72b8771767f
Tshep45 <tshepomakama45@gmail.com>
2023-03-14 12:10:10 +0200

    Fixed linters error

--- script.js
+++ script.js
@@ -6,24 +6,8 @@ hamburger.addEventListener('click', () => {
   navMenu.classList.toggle('active');
 })
 
-<<<<<<< HEAD
 document.querySelectorAll('.nav-link').forEach(n => n.
   addEventListener('click', () => {
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
  })) 
-=======
-document.querySelectorAll(".nav-link").forEach(n => n.
- addEventListener("click" , () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-    
- }))
-
-
- 
- 
- 
- 
- 
->>>>>>> 4493674b9e7fa56766a8a07bb245e5a1839d5bc5
